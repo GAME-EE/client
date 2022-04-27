fd300b4249c73a7adaf9570b3f8690b4a0f386dc
정현수 <jung660317@naver.com>
2022-04-27 23:14:08 +0900

    chore: 불필요한 주석 제거

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,6 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import { CssBaseline } from '@nextui-org/react';
 
-//TODO: 에러해결하자 현수야
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
