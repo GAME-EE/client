c3b6f79ee419b9160477d06efb0e54f19388c736
sumi.byun <selina2000@naver.com>
2022-04-27 23:59:21 +0900

    feat : canvas 생성

--- src/pages/game/dyno.tsx
+++ src/pages/game/dyno.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from 'next';
-
+import { useEffect, useRef } from 'react';
 const Dyno: NextPage = () => {
-  return <div>dyno</div>;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    if (canvasRef != undefined) {
+      const canvas: any = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      console.log(canvas);
+    }
+  }, []);
+
+  return (
+    <div>
+      dyno
+      <div style={{ width: '700px', height: '400px', backgroundColor: '#afafaf' }}>
+        canvas
+        <canvas ref={canvasRef} style={{ width: '600px', height: '300px' }}></canvas>
+      </div>
+    </div>
+  );
 };
 
 export default Dyno;
