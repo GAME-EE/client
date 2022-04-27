44733a6c30a3ccc667d8167c2df3dc242c0d899e
정현수 <jung660317@naver.com>
2022-04-27 22:00:07 +0900

    chore: 테스트 커밋
    - TODO 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import { CssBaseline } from '@nextui-org/react';
 
+//TODO: 에러해결하자 현수야
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
