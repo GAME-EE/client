7aaf26e40e89022a7656c0b7299367d0e07ed203
정현수 <jung660317@naver.com>
2022-04-27 23:13:26 +0900

    chore: Document 타입 에러 해결
    - styles 프로퍼티를 배열 처리

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
     const initialProps = await Document.getInitialProps(ctx);
     return {
       ...initialProps,
-      styles: <>{initialProps.styles}</>,
+      styles: [<>{initialProps.styles}</>],
     };
   }
 
