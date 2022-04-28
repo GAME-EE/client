db0af5f9e58ff31d1081382532a75b31bf34505d
sumi.byun <selina2000@naver.com>
2022-04-29 01:09:36 +0900

    fix: canvas...
    왜 캔버스가 그려지지 않을까

--- src/pages/game/dyno.tsx
+++ src/pages/game/dyno.tsx
@@ -5,23 +5,42 @@ const Dyno: NextPage = () => {
   useEffect(() => {
     // if (canvasRef != undefined) {
     if (!canvasRef.current) {
+      console.log('canvasRef.current');
+
       return;
     }
-    const canvas: HTMLCanvasElement = canvasRef.current;
-    const context = canvas.getContext('2d');
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    // const canvas: HTMLCanvasElement = canvasRef.current;
+    canvas.width = 500;
+    canvas.height = 500;
 
-    console.log(context);
+    const ctx = canvas.getContext('2d');
+    console.log(ctx);
+    if (!ctx) {
+      console.log('ctx');
+      return;
+    }
+    ctx.fillStyle = 'green';
+    ctx.fillRect(10, 10, 100, 100);
 
     // }
   }, []);
-
+  // const dino = {
+  //   x: 10,
+  //   y: 200,
+  //   width: 50,
+  //   height: 50,
+  //   draw() {
+  //     ctx.fillStyle = 'green';
+  //     ctx.fillReact(this.x, this.y, this.width, this.height);
+  //   },
+  // };
   return (
     <div>
       dyno
-      <div style={{ width: '700px', height: '400px', backgroundColor: '#afafaf' }}>
-        canvas
-        <canvas ref={canvasRef} style={{ width: '600px', height: '300px' }}></canvas>
-      </div>
+      {/* <div style={{ width: '700px', height: '400px', backgroundColor: '#afafaf' }}> */}
+      <canvas ref={canvasRef} id="canvas"></canvas>
+      {/* </div> */}
     </div>
   );
 };
