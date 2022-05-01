5a831786c80960060844dac7f53e36af5bbdc8bf
sumi.byun <selina2000@naver.com>
2022-05-02 01:00:51 +0900

    feeat : canvas not working

--- src/pages/game/dyno.tsx
+++ src/pages/game/dyno.tsx
@@ -1,46 +1,10 @@
 import type { NextPage } from 'next';
-import { useEffect, useRef } from 'react';
+import DynoCanvas from '../../components/DynoCanvas';
 const Dyno: NextPage = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  useEffect(() => {
-    // if (canvasRef != undefined) {
-    if (!canvasRef.current) {
-      console.log('canvasRef.current');
-
-      return;
-    }
-    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-    // const canvas: HTMLCanvasElement = canvasRef.current;
-    canvas.width = 500;
-    canvas.height = 500;
-
-    const ctx = canvas.getContext('2d');
-    console.log(ctx);
-    if (!ctx) {
-      console.log('ctx');
-      return;
-    }
-    ctx.fillStyle = 'green';
-    ctx.fillRect(10, 10, 100, 100);
-
-    // }
-  }, []);
-  // const dino = {
-  //   x: 10,
-  //   y: 200,
-  //   width: 50,
-  //   height: 50,
-  //   draw() {
-  //     ctx.fillStyle = 'green';
-  //     ctx.fillReact(this.x, this.y, this.width, this.height);
-  //   },
-  // };
   return (
     <div>
       dyno
-      {/* <div style={{ width: '700px', height: '400px', backgroundColor: '#afafaf' }}> */}
-      <canvas ref={canvasRef} id="canvas"></canvas>
-      {/* </div> */}
+      <DynoCanvas />
     </div>
   );
 };
