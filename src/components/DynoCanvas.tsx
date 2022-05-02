30843f25f03953e3d4337e3b17ee0bdcec1b4200
sumi.byun <selina2000@naver.com>
2022-05-02 17:10:18 +0900

    feat : 움직이는 canvas item 생성

--- src/components/DynoCanvas.tsx
+++ src/components/DynoCanvas.tsx
@@ -5,25 +5,69 @@ import React, { useEffect, useRef, useState } from 'react';
 function DynoCanvas({}) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
+  const [dinoObj, setDinoObj] = useState({
+    x: 10,
+    y: 10,
+    width: 20,
+    height: 20,
+  });
+  const [timer, setTimer] = useState(0);
   useEffect(() => {
     setContext(canvasRef.current && canvasRef.current.getContext('2d'));
 
-    context && context.fillRect(25, 25, 100, 100);
+    // var c = new Cactus();
+    if (context) {
+      // context && context.fillRect(10, 20, 50, 50);
+      drawDino(context, dinoObj);
+      drawObstacle(context, { x: 100, y: 120 });
+      frameRepeat();
+    }
   }, [context, canvasRef]);
-  console.log(context, canvasRef);
+  const frameRepeat = () => {
+    window.requestAnimationFrame(frameRepeat);
+    setTimer(timer + 1);
+    if (!context) return;
+
+    context.clearRect(0, 0, 500, 500); //초기화
+
+    if (timer % 120 === 0) {
+      drawObstacle(context, { x: 100, y: 120 });
+    }
+    setDinoObj({ ...dinoObj, x: dinoObj.x++ });
+    drawDino(context, dinoObj);
+  };
 
   return (
     <>
       <canvas
         ref={canvasRef}
         style={{
-          width: `${500}px`,
-          height: `${500}px`,
+          width: `${600}px`,
+          height: `${200}px`,
           border: '0.1px solid black',
         }}
       />
     </>
   );
 }
+const drawDino = (context: CanvasRenderingContext2D, dino) => {
+  console.log(context);
+
+  if (context) {
+    context.fillRect(dino.x, dino.y, dino.width, dino.height);
+  }
+};
+const drawObstacle = (context: CanvasRenderingContext2D, obj: { x: number; y: number }) => {
+  const obstacle = {
+    x: obj.x,
+    y: obj.y,
+    width: 20,
+    height: 30,
+  };
+  if (context) {
+    context.fillStyle = 'green';
+    context.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+  }
+};
 
 export default DynoCanvas;
