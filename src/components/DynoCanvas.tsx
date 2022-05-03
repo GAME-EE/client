b3c872a430c8385d2998aa696b7a730acdd43277
sumi.byun <selina2000@naver.com>
2022-05-04 01:13:21 +0900

    fix : styled 수정

--- src/components/DynoCanvas.tsx
+++ src/components/DynoCanvas.tsx
@@ -1,32 +1,37 @@
 // import React from 'react';
-
+import { Button, Center, Flex } from '@chakra-ui/react';
 import { useCallback, useEffect, useRef, useState } from 'react';
+
+//불변값들
 const OBSTACLE_OBJECT = {
   y: 130,
   width: 20,
   height: 20,
-  maxY: 60,
+  color: '#6B46C1',
 };
 const CANVAS_OBJECT = {
+  //height - OBSTACLE_OBJECT.height = 120
   height: 140,
   // maxHeight: 120,
 };
 const DINO_OBJECT = {
+  //y+width = 150
   x: 20,
   y: 120,
   width: 30,
   height: 30,
+  maxY: 60,
 };
 const DINO_SPEED = 3;
 const OBSTACLE_SPEED = 2;
 const STAY_MAX_TIME = 5;
+
 const DynoCanvas = ({}) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
-  // const [dinoYRef, setdinoYRefRef] = useState(120);
   const obstacleRef = useRef<number[]>([]);
-  const dinoYRef = useRef<number>(120);
-  // const jumpRef = useRef<boolean>(false);/
+  // const [playing, setplaying] = useState(false);
+  const dinoYRef = useRef<number>(DINO_OBJECT.y);
   let jumping: boolean = false;
   let timer = 0;
   let stayTime = 0;
@@ -39,7 +44,7 @@ const DynoCanvas = ({}) => {
     };
 
     if (!context) return;
-    context.fillStyle = 'green';
+    context.fillStyle = 'pink';
     context.fillRect(rect.x, rect.y, rect.width, rect.height);
   }, [context, dinoYRef]);
 
@@ -62,8 +67,10 @@ const DynoCanvas = ({}) => {
       const tempX = 300;
       obstacleRef.current = [...obstacleRef.current, tempX];
     }
+
     // NOTE: 장애물들을 프레임마다 1씩 줄이고
     obstacleRef.current = obstacleRef.current.map(i => i - OBSTACLE_SPEED);
+
     // NOTE: 1씩 줄였으니까, 다시 그려줘야 함
     obstacleRef.current.forEach((value, i, o) => {
       if (value < 0 - OBSTACLE_OBJECT.width) {
@@ -71,33 +78,29 @@ const DynoCanvas = ({}) => {
       }
       drawObstacleToX(context, value);
     });
+
     //체공 상태인 경우 dinoYRef.current === OBSTACLE_OBJECT.maxY
-    if (dinoYRef.current === OBSTACLE_OBJECT.maxY) {
+    if (dinoYRef.current === DINO_OBJECT.maxY) {
       // console.log('체공');
       stayTime++;
     }
+
     //체공 상태에서 끝나, 점프를 멈추어야 하는 상태
     if (stayTime > STAY_MAX_TIME) {
-      // console.log('점프 stop');
+      console.log('점프 stop');
       jumping = false;
       stayTime = 0;
     }
+
     //점프를 해야하는 상태
-    if (dinoYRef.current > OBSTACLE_OBJECT.maxY && jumping == true) {
-      // console.log('점프');
+    if (dinoYRef.current > DINO_OBJECT.maxY && jumping == true) {
+      console.log('점프');
       dinoYRef.current = dinoYRef.current - DINO_SPEED;
     }
-    //점프후 내려가는 상태, 내려갈수 있는 한계를 지정
-    // if (dinoYRef.current < OBSTACLE_OBJECT.maxY) {
-    //   jumping = false;
-    // }
-    // else if (jumping == true) {
-    //   dinoYRef.current = dinoYRef.current - DINO_SPEED;
-    // }
 
     //점프 상태가 아닌때 아래로, 내려갈수 있는 한계를 지정
     if (jumping == false && dinoYRef.current < CANVAS_OBJECT.height - OBSTACLE_OBJECT.height) {
-      // console.log('내려감');
+      console.log('내려감');
       //캔버스 아래 끝까지만 내려가도록,
       dinoYRef.current = dinoYRef.current + DINO_SPEED;
     }
@@ -107,7 +110,7 @@ const DynoCanvas = ({}) => {
 
   const drawObstacleToX = (ctx: CanvasRenderingContext2D | null, x: number) => {
     if (!ctx) return;
-
+    ctx.fillStyle = OBSTACLE_OBJECT.color;
     ctx.fillRect(x, OBSTACLE_OBJECT.y, OBSTACLE_OBJECT.width, OBSTACLE_OBJECT.height);
   };
 
@@ -123,7 +126,7 @@ const DynoCanvas = ({}) => {
     }
   };
   return (
-    <div>
+    <Center flexDirection={'column'} m={10}>
       <canvas
         ref={canvasRef}
         style={{
@@ -132,9 +135,17 @@ const DynoCanvas = ({}) => {
           border: '0.1px solid black',
         }}
       />
-      <button onClick={byFrame}>start</button>
-      <button onClick={handleJump}>jump</button>
-    </div>
+      <Center>
+        <Button colorScheme="purple" onClick={byFrame} m={5}>
+          start
+        </Button>
+        <Button colorScheme="purple" onClick={handleJump} m={5}>
+          jump
+        </Button>
+      </Center>
+      {/* <button onClick={byFrame}>start</button>
+      <button onClick={handleJump}>jump</button> */}
+    </Center>
   );
 };
 
