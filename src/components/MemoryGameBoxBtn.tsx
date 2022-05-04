6bd03099888984a9570be199433a63dd7843a750
bae-sh <hyeon1293@gmail.com>
2022-05-04 10:58:01 +0900

    feat : loading 시 클릭 방지 구현

--- src/components/MemoryGameBoxBtn.tsx
+++ src/components/MemoryGameBoxBtn.tsx
@@ -4,25 +4,31 @@ import { useEffect } from 'react';
 interface IProps {
   changedColor: string;
   count: number;
+  isLoading: boolean;
+  setIsLoading: any;
 }
-const MemoryGameBoxBtn = ({ changedColor, count }: IProps) => {
+const MemoryGameBoxBtn = ({ changedColor, count, isLoading, setIsLoading }: IProps) => {
   const [bgColor, setBgColor] = useState<string>('blackAlpha.50');
   const [bgHoverColor, setBgHoverColor] = useState<string>('blackAlpha.300');
-  console.log(count);
   useEffect(() => {
     if (count !== -1) {
       setTimeout(() => {
         setBgColor('blue.400');
-      }, 600 * count);
+      }, 700 * count);
       setTimeout(() => {
         setBgColor('blackAlpha.50');
-      }, 600 * (count + 1));
+        if (count == 5) {
+          setIsLoading(false);
+        }
+      }, 700 * (count + 1));
     }
-  }, [count]);
+  }, [count, setIsLoading]);
   const handleButtonClick = useCallback(() => {
-    setBgColor(changedColor);
-    setBgHoverColor(changedColor);
-  }, [changedColor]);
+    if (!isLoading) {
+      setBgColor(changedColor);
+      setBgHoverColor(changedColor);
+    }
+  }, [changedColor, isLoading]);
   return (
     <GridItem
       w="100%"
@@ -30,8 +36,8 @@ const MemoryGameBoxBtn = ({ changedColor, count }: IProps) => {
       bg={bgColor}
       borderRadius={'4px'}
       _hover={{
-        bgColor: bgHoverColor,
-        cursor: bgColor === 'blackAlpha.50' ? 'pointer' : 'default',
+        bgColor: isLoading ? 'none' : bgHoverColor,
+        cursor: isLoading ? 'default' : 'pointer',
       }}
       onClick={handleButtonClick}
     />
