c8d69efdb3135968cf79a176a446d2fa07eb9abe
bae-sh <hyeon1293@gmail.com>
2022-05-11 19:08:37 +0900

    feat : useScore 훅 생성

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,4 +1,5 @@
 import useBgColor from './useBgColor';
 import useStage from './useStage';
+import useTimer from './useTimer';
 // TODO: custom hooks 생성
-export { useBgColor, useStage };
+export { useBgColor, useStage, useTimer };
