d4514afd8e1486fee30ff5dde896cd6203079b8c
정현수 <jung660317@naver.com>
2022-05-11 17:48:24 +0900

    chore: 매직 넘버 상수화

--- src/constants/snake.ts
+++ src/constants/snake.ts
@@ -6,6 +6,9 @@ export default Object.freeze({
   // 단위
   SNAKE_BODY_WIDTH_UNIT: 10,
   SNAKE_BODY_HEIGHT_UNIT: 5,
+  FRAME_DOWN_UNIT: 1,
+  FRAME_DOWN_LIMIT: 50,
+  TICK_TOC_UNIT: 10,
 
   // 색
   SNAKE_BODY_COLOR: '#f5046d',
