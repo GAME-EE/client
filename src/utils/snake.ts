c72b61d4f1ded893ba3480f4e671ff9bf700ce36
정현수 <jung660317@naver.com>
2022-05-12 21:11:26 +0900

    chore: 뱀 게임 유틸 함수 매개변수명 변경
    - 조금 더 직관적으로 변경

--- src/utils/snake.ts
+++ src/utils/snake.ts
@@ -3,22 +3,24 @@ import { SNAKE } from '../constants';
 import type { IObjectBody } from '../types/canvas';
 
 export const isSnakeCollided = (
-  snake: IObjectBody[],
-  currentSnakeHeadPos: IObjectBody,
+  SnakeBodyPosition: IObjectBody[],
+  SnakeHeadPosition: IObjectBody,
 ): boolean => {
-  for (let i = 0; i < snake.length; i++) {
+  for (let i = 0; i < SnakeBodyPosition.length; i++) {
     const isCurrentCollided =
-      snake[i].x === currentSnakeHeadPos.x && snake[i].y === currentSnakeHeadPos.y && i !== 0;
+      SnakeBodyPosition[i].x === SnakeHeadPosition.x &&
+      SnakeBodyPosition[i].y === SnakeHeadPosition.y &&
+      i !== 0;
     if (isCurrentCollided) return true;
   }
   return false;
 };
 
-export const isSnakeOutOfCanvas = (currentSnakeHeadPos: IObjectBody): boolean => {
+export const isSnakeOutOfCanvas = (snakeHeadPosition: IObjectBody): boolean => {
   return (
-    currentSnakeHeadPos.x < SNAKE.CANVAS_REAL_WIDTH_MIN ||
-    currentSnakeHeadPos.y < SNAKE.CANVAS_REAL_HEIGHT_MIN ||
-    currentSnakeHeadPos.x > SNAKE.CANVAS_REAL_WIDTH_MAX ||
-    currentSnakeHeadPos.y > SNAKE.CANVAS_REAL_HEIGHT_MAX
+    snakeHeadPosition.x < SNAKE.CANVAS_REAL_WIDTH_MIN ||
+    snakeHeadPosition.y < SNAKE.CANVAS_REAL_HEIGHT_MIN ||
+    snakeHeadPosition.x > SNAKE.CANVAS_REAL_WIDTH_MAX ||
+    snakeHeadPosition.y > SNAKE.CANVAS_REAL_HEIGHT_MAX
   );
 };
