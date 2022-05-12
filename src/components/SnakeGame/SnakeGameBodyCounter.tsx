8a9e6d0e847933769f80826a46f17201a927be66
정현수 <jung660317@naver.com>
2022-05-12 20:54:42 +0900

    chore: SnakeGameBodyCounter Progressbar 제거

--- src/components/SnakeGame/SnakeGameBodyCounter.tsx
+++ src/components/SnakeGame/SnakeGameBodyCounter.tsx
@@ -1,15 +1,10 @@
 import { memo } from 'react';
-import { Progress, Box } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 import type { ISnakeGameHook } from '../../types/snake';
 
 const SnakeGameBodyCounter = ({ snakeBodyLength }: Pick<ISnakeGameHook, 'snakeBodyLength'>) => {
-  return (
-    <Box>
-      {snakeBodyLength}
-      <Progress width="360px" colorScheme="purple" hasStripe value={snakeBodyLength} />
-    </Box>
-  );
+  return <Box>{snakeBodyLength}</Box>;
 };
 
 export default memo(SnakeGameBodyCounter);
