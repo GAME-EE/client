62984df42f048d12dbf4a9522e79e74e62e13a5d
정현수 <jung660317@naver.com>
2022-05-26 23:17:49 +0900

    feat: footer 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,9 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Box } from '@chakra-ui/react';
 
-import { HomeDownArrow, HomeTitleText } from '../components/';
-// import HomeGameButton from '../components/HomeGameButton';
+import { HomeGameButton, HomeDownArrow, HomeTitleText } from '../components/';
 
-// import { ROUTES } from '../constants';
+import { ROUTES } from '../constants';
 
 const Home: NextPage = () => {
   return (
@@ -16,38 +15,70 @@ const Home: NextPage = () => {
         <meta name="description" content="Game-ee" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box as="header"></Box>
       <Box
-        display="flex"
-        flexDirection="column"
-        backgroundColor="blackAlpha.900"
-        rowGap="100px"
-        justifyContent="center"
-        alignItems="center"
-        width="100vw"
-        height="100vh"
         as="main"
+        justifyContent="center"
+        flexDirection="column"
+        alignItems="center"
+        display="flex"
       >
-        <HomeTitleText />
+        <Box as="header"></Box>
         <Box
-          as="article"
+          as="section"
           display="flex"
-          width={{ base: '80vw', tablet: '360px' }}
+          flexDirection="column"
+          backgroundColor="blackAlpha.900"
+          rowGap="100px"
           justifyContent="center"
-          borderBottom="3px solid white"
-          alignItems="baseline"
+          alignItems="center"
+          width="100%"
+          height="100vh"
+        >
+          <HomeTitleText />
+          <Box
+            as="article"
+            display="flex"
+            width={{ base: '80vw', tablet: '360px' }}
+            justifyContent="center"
+            borderBottom="7px solid white"
+            alignItems="baseline"
+            columnGap="30px"
+            rowGap="30px"
+          >
+            <Image src="/chick.png" alt="chicken" width="50px" height="50px" draggable={false} />
+            <Image src="/dino1.png" alt="dino" width="75px" height="75px" draggable={false} />
+          </Box>
+          <HomeDownArrow />
+        </Box>
+        <Box
+          as="section"
+          display="flex"
+          flexDirection={{ base: 'column', tablet: 'row' }}
+          backgroundColor="blackAlpha.700"
           columnGap="30px"
           rowGap="30px"
+          justifyContent="center"
+          alignItems="center"
+          width="100%"
+          height="100vh"
         >
-          <Image src="/chick.png" alt="chicken" width="50px" height="50px" draggable={false} />
-          <Image src="/dino1.png" alt="dino" width="75px" height="75px" draggable={false} />
-          {/* <HomeGameButton href={ROUTES.DYNO}>다이노</HomeGameButton>
+          <HomeGameButton href={ROUTES.DYNO}>다이노</HomeGameButton>
           <HomeGameButton href={ROUTES.SNAKE}>뱀</HomeGameButton>
-          <HomeGameButton href={ROUTES.MEMORY}>기억력</HomeGameButton> */}
+          <HomeGameButton href={ROUTES.MEMORY}>기억력</HomeGameButton>
         </Box>
-        <HomeDownArrow />
+        <Box
+          as="footer"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          width="100%"
+          backgroundColor="blackAlpha.900"
+          color="white"
+          height="200px"
+        >
+          FOOTER
+        </Box>
       </Box>
-      <Box as="footer"></Box>
     </div>
   );
 };
