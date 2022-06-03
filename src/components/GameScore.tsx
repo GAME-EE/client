0818d005f6b29a7bced9e126c0c5a5f22d11ef93
정현수 <jung660317@naver.com>
2022-06-03 23:44:53 +0900

    feat: GameScore 컴포넌트 생성

---
+++ src/components/GameScore.tsx
@@ -0,0 +1,31 @@
+import { memo } from 'react';
+import { motion } from 'framer-motion';
+
+import CustomChakraMotion from './CustomChakraMotion';
+
+const Score = CustomChakraMotion(motion.div);
+
+interface GameScoreInterface {
+  score: number;
+}
+
+/**
+ * @description
+ * 게임 스코어가 바뀔 때 애니메이션 실행되는 컴포넌트
+ *
+ * usage: `<GameScore score={number} />`
+ */
+const GameScore = ({ score }: GameScoreInterface) => {
+  return (
+    <Score
+      fontSize={26}
+      key={score}
+      initial={{ rotate: 50, opacity: 0 }}
+      animate={{ scale: 1.5, rotate: 0, opacity: 1 }}
+    >
+      {score.toLocaleString('ko-KR')}
+    </Score>
+  );
+};
+
+export default memo(GameScore);
