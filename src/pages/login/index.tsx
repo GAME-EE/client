e439352509562e5a550c18d6abdbb276a3a78b9e
bae-sh <hyeon1293@gmail.com>
2022-06-28 02:30:28 +0900

    feat : 카카오 로그인 구현

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,40 +1,10 @@
-import { Box, Button, Text } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import type { NextPage } from 'next';
-import Image from 'next/image';
-
 import { Header, Footer, HomeTitleText } from '../../components';
+import SNSLoginButton from '../../components/Login/SNSLoginButton';
+import { LOGIN_TYPE } from '../../constants';
 
 const Login: NextPage = () => {
-  const LOGIN_TYPE = [
-    {
-      name: 'kakao',
-      img: '/login/kakao.png',
-      title: '카카오 로그인',
-      bg: '#FEE500',
-      color: 'black',
-    },
-    {
-      name: 'naver',
-      img: '/login/naver.png',
-      title: '네이버 로그인',
-      bg: '#03C85A',
-      color: 'black',
-    },
-    {
-      name: 'facebook',
-      img: '/login/facebook.png',
-      title: '페이스북 로그인',
-      bg: '#3B5B98',
-      color: 'white',
-    },
-    {
-      name: 'github',
-      img: '/login/github.png',
-      title: '깃허브 로그인',
-      bg: '#374051',
-      color: 'white',
-    },
-  ];
   return (
     <main>
       <Header isVisible={true} />
@@ -46,25 +16,9 @@ const Login: NextPage = () => {
         flexDirection="column"
       >
         <HomeTitleText color="black" />
-        {LOGIN_TYPE.map(type => {
-          return (
-            <Button
-              bg={type.bg}
-              color={type.color}
-              height="50px"
-              width="300px"
-              fontSize="24px"
-              as="button"
-              _hover={{ opacity: 0.7 }}
-              // onClick={onClickStartBtn}
-              marginTop="20px"
-              key={type.name}
-            >
-              <Image src={type.img} alt={type.name} width="24px" height="24px" draggable={false} />
-              <Text ml="10px">{type.title}</Text>
-            </Button>
-          );
-        })}
+        {LOGIN_TYPE.map(type => (
+          <SNSLoginButton type={type} key={type.name} />
+        ))}
       </Box>
       <Footer />
     </main>
