8dafa266d04d10e95a12a741a83ac3efd3c6e4d5
bae-sh <hyeon1293@gmail.com>
2022-07-08 01:27:49 +0900

    깃허브 페이스북 로그인 버튼 제거

--- src/constants/login.ts
+++ src/constants/login.ts
@@ -1,38 +1,24 @@
 import { KAKAO_AUTH_URL } from '../api/oauth';
 
-const LOGIN_TYPE = [
-  {
+const LOGIN_TYPE = Object.freeze({
+  kakao: {
     name: 'kakao',
     img: '/login/kakao.png',
     title: '카카오 로그인',
     bg: '#FEE500',
     color: 'black',
     uri: KAKAO_AUTH_URL,
+    id: 'kakaoIdLogin',
   },
-  {
+  naver: {
     name: 'naver',
     img: '/login/naver.png',
     title: '네이버 로그인',
     bg: '#03C85A',
     color: 'black',
-    uri: KAKAO_AUTH_URL,
+    uri: '',
+    id: 'naverIdLogin',
   },
-  {
-    name: 'facebook',
-    img: '/login/facebook.png',
-    title: '페이스북 로그인',
-    bg: '#3B5B98',
-    color: 'white',
-    uri: KAKAO_AUTH_URL,
-  },
-  {
-    name: 'github',
-    img: '/login/github.png',
-    title: '깃허브 로그인',
-    bg: '#374051',
-    color: 'white',
-    uri: KAKAO_AUTH_URL,
-  },
-];
+});
 
 export default LOGIN_TYPE;
