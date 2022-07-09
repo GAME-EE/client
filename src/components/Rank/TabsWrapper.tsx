6475f7987dfe9ffe1d97b93cfc0eb306138dc338
sumi.byun <selina2000@naver.com>
2022-07-09 23:38:02 +0900

    fix : fix bg style

--- src/components/Rank/TabsWrapper.tsx
+++ src/components/Rank/TabsWrapper.tsx
@@ -6,15 +6,21 @@ import { ELEMENT_COLOR } from '../../styles/colors';
 function TabsWrapper({ data, children }: { data: Array<any>; children: React.ReactNode }) {
   return (
     <Tabs
-      backgroundColor={ELEMENT_COLOR.HOME_SECOND_BG_COLOR}
+      backgroundColor={ELEMENT_COLOR.HOME_MAIN_BG_COLOR}
       borderColor={ELEMENT_COLOR.HOME_MAIN_BG_COLOR}
       w={'100%'}
       outline={'none'}
-      isFitted
+      // isFitted
       variant="enclosed"
       h={'100vh'}
     >
-      <TabList mb="1em" marginTop={'10px'}>
+      <TabList
+        mb="1em"
+        p={2}
+        backgroundColor={ELEMENT_COLOR.HOME_SECOND_BG_COLOR}
+        justifyContent={'center'}
+        gap={10}
+      >
         {data.map(({ name }) => (
           <TabWrapper key={name} name={name + ''} />
         ))}
@@ -27,8 +33,9 @@ const TabWrapper = ({ name }: { name: string }) => {
   return (
     <Tab
       _selected={{
-        borderColor: ELEMENT_COLOR.HOME_MAIN_BG_COLOR,
-        borderBottomColor: ELEMENT_COLOR.HOME_SECOND_BG_COLOR,
+        // borderColor: ELEMENT_COLOR.HOME_MAIN_BG_COLOR,
+        // borderBottomColor: ELEMENT_COLOR.HOME_SECOND_BG_COLOR,
+        color: '#fff',
       }}
       _focus={{
         outline: 'none',
--- src/pages/rank.tsx
+++ src/pages/rank.tsx
@@ -5,13 +5,13 @@ import TopItemWrapper from '../components/Rank/TopItemWrapper';
 import BottomItemWrapper from '../components/Rank/BottomItemWrapper';
 import { DUMMY1, DUMMY2, DUMMY3 } from '../constants/rank';
 import TabsWrapper from '../components/Rank/TabsWrapper';
-const DUMMYTAP = [{ name: 1 }, { name: 2 }, { name: 3 }];
+const DUMMYTAP = [{ name: 'dyno' }, { name: 'memory' }, { name: 'snake' }];
 const Rank = () => {
   return (
     <Wrapper>
       <TabsWrapper data={DUMMYTAP}>
         {[DUMMY1, DUMMY2, DUMMY3].map((item, idx) => (
-          <TabPanel key={idx} marginX={48}>
+          <TabPanel key={idx} marginX={'auto'} maxWidth={'768px'}>
             <TopItemWrapper data={item.slice(0, 3)} idx={idx} />
             <BottomItemWrapper data={item.slice(3)} />
           </TabPanel>
