fa8df82a1d78c2178c56302a94041a539d4d2a52
sumi.byun <selina2000@naver.com>
2022-07-10 16:32:54 +0900

    fix : motion props move

--- src/constants/rank.ts
+++ src/constants/rank.ts
@@ -1,3 +1,33 @@
+export const BottonItemWrapperMotion = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 1,
+      staggerChildren: 0.2,
+    },
+  },
+};
+export const TopItemWrapperMotion = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+export const ItemMotion = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
 export const DUMMY1 = [
   {
     id: 1,
