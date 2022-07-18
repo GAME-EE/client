bc3e0a8d80099e7554d3f6a79ff011ad58024596
bae-sh <hyeon1293@gmail.com>
2022-07-18 18:52:11 +0900

    feat : score값을 useState로 변경

--- src/hooks/useScore.ts
+++ src/hooks/useScore.ts
@@ -1,7 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useCallback } from 'react';
-import { useRecoilState } from 'recoil';
-import { scoreState } from '../atom';
+import { useCallback, useState } from 'react';
 import { stringToNumber, numberToString } from '../utils/number';
 
 export interface TimerHookProps {
@@ -11,7 +9,7 @@ export interface TimerHookProps {
 }
 
 function useScore(): TimerHookProps {
-  const [score, setScore] = useRecoilState<string>(scoreState);
+  const [score, setScore] = useState('0');
 
   const plusScore = useCallback(
     (amount: number) => {
