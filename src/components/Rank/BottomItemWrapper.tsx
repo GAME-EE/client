bb4b9d76c404dc3c7faab5e19e1d9bb1e4c8a74f
sumi.byun <selina2000@naver.com>
2022-07-20 00:50:55 +0900

    flood : rendering error fixing

--- src/components/Rank/BottomItemWrapper.tsx
+++ src/components/Rank/BottomItemWrapper.tsx
@@ -1,18 +1,23 @@
-import { Avatar, chakra } from '@chakra-ui/react';
+import { Avatar, Box, chakra } from '@chakra-ui/react';
 import { ELEMENT_COLOR } from '../../styles/colors';
 import { motion, isValidMotionProp } from 'framer-motion';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { ItemMotion, BottonItemWrapperMotion } from '../../constants/rank';
 
 interface IBottomItemWraer {
   data: Array<any>;
+  handleCardOpen: any;
   idx: any;
 }
-function BottomItemWrapper({ data }: IBottomItemWraer) {
+function BottomItemWrapper({ data, handleCardOpen }: IBottomItemWraer) {
   // NOTE :왜 이 코드를 안에 두어야 챕을 옮길때마가 모션이 다시 실행되는걸까요. 이유?!
-  const MotionDiv = chakra(motion.div, {
-    shouldForwardProp: prop => isValidMotionProp(prop) || prop === 'children',
-  });
+
+  const MotionDiv = useCallback(
+    chakra(motion.div, {
+      shouldForwardProp: prop => isValidMotionProp(prop) || prop === 'children',
+    }),
+    [data],
+  );
 
   return (
     <MotionDiv
@@ -26,23 +31,21 @@ function BottomItemWrapper({ data }: IBottomItemWraer) {
       animate="visible"
     >
       {data.map((item, idx) => (
-        <MotionDiv
-          display={'flex'}
-          h="50px"
-          lineHeight={'50px'}
-          key={idx}
-          gap={8}
-          variants={ItemMotion}
-          color={'#fff'}
-        >
-          <span>{item.index}</span>
-          <Avatar name={item.name} color={'#fff'} bg={ELEMENT_COLOR.HOME_SECOND_BG_COLOR}></Avatar>
-          <span>{item.name}</span>
-          <span>{item.score}</span>
+        <MotionDiv h="50px" lineHeight={'50px'} key={idx} variants={ItemMotion} color={'#fff'}>
+          <Box onClick={() => handleCardOpen(item.name + idx)} gap={8} display={'flex'}>
+            <span>{item.index}</span>
+            <Avatar
+              name={item.name}
+              color={'#fff'}
+              bg={ELEMENT_COLOR.HOME_SECOND_BG_COLOR}
+            ></Avatar>
+            <span>{item.name}</span>
+            <span>{item.score}</span>
+          </Box>
         </MotionDiv>
       ))}
     </MotionDiv>
   );
 }
 
-export default BottomItemWrapper;
+export default React.memo(BottomItemWrapper);
--- src/components/Rank/TopItemWrapper.tsx
+++ src/components/Rank/TopItemWrapper.tsx
@@ -1,7 +1,7 @@
 import { Avatar, Box, chakra } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { motion, isValidMotionProp } from 'framer-motion';
+import { motion, isValidMotionProp, AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
 
 import { ELEMENT_COLOR } from '../../styles/colors';
@@ -10,61 +10,86 @@ import { CROWN_ORDER, ItemMotion, TopItemWrapperMotion } from '../../constants/r
 interface ITopItemWrapper {
   data: Array<any>;
   idx: any;
+  handleCardOpen: (arg0: string) => void;
 }
 
-const TopItemWrapper = ({ data }: ITopItemWrapper) => {
+const TopItemWrapper = ({ data, handleCardOpen }: ITopItemWrapper) => {
+  const [selectedId, setSelectedId] = useState<null | number>(null);
+
   const MotionDiv = chakra(motion.div, {
     shouldForwardProp: prop => isValidMotionProp(prop) || prop === 'children',
   });
   return (
-    <MotionDiv
-      display="flex"
-      alignItems={'flex-end'}
-      gap={5}
-      h="fit-content"
-      marginY={10}
-      marginBottom={16}
-      variants={TopItemWrapperMotion}
-      initial="hidden"
-      animate="visible"
-    >
-      {data.map((item, idx) => (
-        <AvatarWrapper flex={CROWN_ORDER[idx].size} key={idx} order={CROWN_ORDER[idx].order}>
-          <MotionDiv
-            position="absolute"
-            w="100%"
-            height={'100%'}
-            variants={ItemMotion}
-            whileHover={{ scale: 1.03 }}
-            color={'#fff'}
+    <>
+      <MotionDiv
+        display="flex"
+        alignItems={'flex-end'}
+        gap={5}
+        h="fit-content"
+        marginY={10}
+        marginBottom={16}
+        variants={TopItemWrapperMotion}
+        initial="hidden"
+        animate="visible"
+      >
+        {data.map((item, idx) => (
+          <AvatarWrapper
+            flex={CROWN_ORDER[idx].size}
+            key={idx}
+            order={CROWN_ORDER[idx].order}
+            layoutId={idx}
+            onClick={() => handleCardOpen(item.name + idx)}
+            // onClick={() => setSelectedId(idx + 1)}
           >
-            <Avatar
-              size="full"
-              name={item.content}
-              bg={ELEMENT_COLOR.HOME_SECOND_BG_COLOR}
+            <MotionDiv
+              position="absolute"
+              w="100%"
+              height={'100%'}
+              variants={ItemMotion}
+              whileHover={{ scale: 1.03 }}
               color={'#fff'}
-            ></Avatar>
-            <Box w="fit-content" top={'-40px'} left={0} right={0} m={'0 auto'} position="absolute">
-              <Image
-                src={CROWN_ORDER[idx].img}
-                alt="crown"
-                width="60px"
-                height="50px"
-                draggable={false}
-              />
-            </Box>
-            <Box bottom={-8} w="fit-content" left={0} right={0} m={'0 auto'} position="absolute">
-              {item.name}
-            </Box>{' '}
-            <Box bottom={-14} w="fit-content" left={0} right={0} m={'0 auto'} position="absolute">
-              {item.score}
-            </Box>
-          </MotionDiv>
-        </AvatarWrapper>
-      ))}
-    </MotionDiv>
+            >
+              <Avatar
+                size="full"
+                name={item.content}
+                bg={ELEMENT_COLOR.HOME_SECOND_BG_COLOR}
+                color={'#fff'}
+              ></Avatar>
+              <Box
+                w="fit-content"
+                top={'-40px'}
+                left={0}
+                right={0}
+                m={'0 auto'}
+                position="absolute"
+              >
+                <Image
+                  src={CROWN_ORDER[idx].img}
+                  alt="crown"
+                  width="60px"
+                  height="50px"
+                  draggable={false}
+                />
+              </Box>
+              <Box bottom={-8} w="fit-content" left={0} right={0} m={'0 auto'} position="absolute">
+                {item.name}
+              </Box>{' '}
+              <Box bottom={-14} w="fit-content" left={0} right={0} m={'0 auto'} position="absolute">
+                {item.score}
+              </Box>
+            </MotionDiv>
+          </AvatarWrapper>
+        ))}
+      </MotionDiv>
+    </>
   );
 };
+const MotionWrapper = styled(Box)`
+  position: fixed;
+  width: 400px;
+  height: 400px;
+  background-color: #fff;
+`;
 const AvatarWrapper = styled(Box)`
   position: relative;
   &:after {
@@ -73,4 +98,4 @@ const AvatarWrapper = styled(Box)`
     padding-bottom: 100%;
   }
 `;
-export default TopItemWrapper;
+export default React.memo(TopItemWrapper);
