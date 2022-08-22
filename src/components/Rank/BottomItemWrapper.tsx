6671edb03e972dc1c59e41e0c7e67d6a04b54c35
sumi.byun <selina2000@naver.com>
2022-08-22 11:05:30 +0900

    fix : SafeHydrate 사용하여 Hydrate 에러 해결
    Hydrate 에러가 왜 나는지는 이유를 찾지 못했음..

--- src/components/Rank/BottomItemWrapper.tsx
+++ src/components/Rank/BottomItemWrapper.tsx
@@ -1,9 +1,10 @@
 import { Avatar, Box, chakra, Td } from '@chakra-ui/react';
 import { ELEMENT_COLOR } from '../../styles/colors';
 import { motion, isValidMotionProp } from 'framer-motion';
-import React, { useCallback } from 'react';
+import React, { useState } from 'react';
 import { ItemMotion, BottonItemWrapperMotion } from '../../constants/rank';
 import { IRankData } from '../../types/rank';
+import SafeHydrate from '../SafeHydrate';
 
 interface IBottomItemWraer {
   data: IRankData[];
@@ -18,37 +19,39 @@ function BottomItemWrapper({ data }: IBottomItemWraer) {
     shouldForwardProp: prop => isValidMotionProp(prop) || prop === 'children',
   });
   return (
-    <MotionDiv
-      display="flex"
-      flexDirection={'column'}
-      marginY={8}
-      paddingY={5}
-      gap={5}
-      variants={BottonItemWrapperMotion}
-      initial="hidden"
-      animate="visible"
-    >
-      <table>
-        {data.map((item: IRankData, idx) => (
-          <MotionTr h="60px" lineHeight={'50px'} key={idx} variants={ItemMotion} color={'#fff'}>
-            <Td w="20px" textAlign="center">
-              {item.rank}
-            </Td>
-            <Td w="100px" textAlign="center">
-              <Avatar
-                name={item.name}
-                color={'#fff'}
-                bg={ELEMENT_COLOR.HOME_SECOND_BG_COLOR}
-              ></Avatar>
-            </Td>
+    <SafeHydrate>
+      <MotionDiv
+        display="flex"
+        flexDirection={'column'}
+        marginY={8}
+        paddingY={5}
+        gap={5}
+        variants={BottonItemWrapperMotion}
+        initial="hidden"
+        animate="visible"
+      >
+        <table>
+          {data.map((item: IRankData, idx) => (
+            <MotionTr h="60px" lineHeight={'50px'} key={idx} variants={ItemMotion} color={'#fff'}>
+              <Td w="20px" textAlign="center">
+                {item.rank}
+              </Td>
+              <Td w="100px" textAlign="center">
+                <Avatar
+                  name={item.name}
+                  color={'#fff'}
+                  bg={ELEMENT_COLOR.HOME_SECOND_BG_COLOR}
+                ></Avatar>
+              </Td>
 
-            <Td w="150px">{item.name}</Td>
-            <Td isNumeric>{item.score}</Td>
-          </MotionTr>
-        ))}
-      </table>
-    </MotionDiv>
+              <Td w="150px">{item.name}</Td>
+              <Td isNumeric>{item.score}</Td>
+            </MotionTr>
+          ))}
+        </table>
+      </MotionDiv>
+    </SafeHydrate>
   );
 }
 
-export default React.memo(BottomItemWrapper);
+export default BottomItemWrapper;
