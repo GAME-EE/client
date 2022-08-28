d4221b104021f04dba3f9e395d136cdb55c77fc3
bae-sh <hyeon1293@gmail.com>
2022-08-28 15:46:22 +0900

    feat : access,refresh 토큰 atom에 저장

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,20 @@ import '../styles/fonts.css';
 import { theme } from '../styles/theme';
 
 import type { AppContext, AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, useSetRecoilState } from 'recoil';
 import cookies from 'next-cookies';
 import { useEffect } from 'react';
+import { token } from '../atom';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const setUserState = useSetRecoilState(token);
+
+  useEffect(() => {
+    const { accessToken, refreshToken } = pageProps;
+
+    setUserState({ accessToken, refreshToken });
+  }, [pageProps, setUserState]);
+
   return (
     <ChakraProvider theme={theme} resetCSS>
       <RecoilRoot>
@@ -22,9 +31,10 @@ MyApp.getInitialProps = async (appContext: AppContext) => {
   const { ctx } = appContext;
   const allCookies = cookies(ctx);
   const accessTokenByCookie = allCookies['access_token'] || '';
+  const refreshTokenByCookie = allCookies['refresh_token'] || '';
   console.log(accessTokenByCookie, 'accessTokenByCookie');
 
-  return { pageProps: { accessTokenByCookie } };
+  return { pageProps: { accessTokenByCookie, refreshTokenByCookie } };
 };
 
 export default MyApp;
