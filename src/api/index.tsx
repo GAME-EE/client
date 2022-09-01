08bd1cf0f69ad09504caafaaa8c54c2fd7de0472
bae-sh <hyeon1293@gmail.com>
2022-09-01 23:23:00 +0900

    chore : getAccessToken 로직 이동

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,2 +1,12 @@
+import axios from 'axios';
+import { getOauthAccessTokenUrl } from './oauth';
 // TODO: api 관련 코드 처리
-export {};
+const getAccessToken = (refreshToken: string | null) => {
+  axios.put(getOauthAccessTokenUrl, {
+    headers: {
+      Authorization: `Bearer ${refreshToken}`,
+    },
+  });
+};
+
+export { getAccessToken };
