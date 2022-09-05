cf68ebe4ba67dcb64b057075d700c01a3036a707
bae-sh <hyeon1293@gmail.com>
2022-09-05 17:36:09 +0900

    fix : install chakra react18

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -2,11 +2,16 @@ import axios from 'axios';
 import { OAUTH_ACCESS_TOKEN_URL } from './oauth';
 // TODO: api 관련 코드 처리
 const getAccessToken = (refreshToken: string | null) => {
-  axios.put(OAUTH_ACCESS_TOKEN_URL, {
-    headers: {
-      Authorization: `Bearer ${refreshToken}`,
+  console.timeLog(refreshToken);
+  axios.put(
+    OAUTH_ACCESS_TOKEN_URL,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${refreshToken}`,
+      },
     },
-  });
+  );
 };
 
 export { getAccessToken };
