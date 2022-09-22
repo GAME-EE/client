91c3592bad110cd565a1b1b8559d1665a28fec9a
bae-sh <hyeon1293@gmail.com>
2022-09-22 20:21:37 +0900

    chore: url 변경

--- src/api/oauth.ts
+++ src/api/oauth.ts
@@ -1,5 +1,5 @@
 const CLIENT_ID = '91f7fc7c7c29a201a48ba81616d60f22';
-const OAUTH_ACCESS_TOKEN_URL = 'http://localhost:4000/api/oauth';
+const OAUTH_ACCESS_TOKEN_URL = 'http://localhost:4000/api/oauth/regenerate/access';
 
 const REDIRECT_URI = 'http://localhost:4000/api/oauth/kakao/sign-up';
 
