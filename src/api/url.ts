c09ae8f39b2253f5436c5722ec1d54ebfff0d8b7
bae-sh <hyeon1293@gmail.com>
2022-10-08 12:56:32 +0900

    chore :baseURL 생성

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,10 +1,12 @@
 const CLIENT_ID = '91f7fc7c7c29a201a48ba81616d60f22';
-const OAUTH_ACCESS_TOKEN_URL = 'http://localhost:4000/api/oauth/regenerate/access';
+const baseURL = 'http://localhost:4000/api';
 
-const REDIRECT_URI = 'http://localhost:4000/api/oauth/kakao/sign-up';
+const OAUTH_ACCESS_TOKEN_URL = `${baseURL}/oauth/regenerate/access`;
+
+const REDIRECT_URI = `${baseURL}/api/oauth/kakao/sign-up`;
 
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
-const GET_USER_PROFILE = 'http://localhost:4000/api/user/profile';
+const GET_USER_PROFILE = `${baseURL}/user/profile`;
 
 export { REDIRECT_URI, KAKAO_AUTH_URL, OAUTH_ACCESS_TOKEN_URL, GET_USER_PROFILE };
